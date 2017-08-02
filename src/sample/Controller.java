334c4b73d3c0a348837e5e6b71108eda138a6a63
Jordan <jordanogilvy@live.com>
2017-08-03 10:53:48 +1200

    basic info class, menu bar calls controller methods

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,35 @@
 package sample;
 
-public class Controller {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+
+import javax.swing.*;
+
+public class Controller{
+
+    private SessionInfo sessionInfo = new SessionInfo();
+
+    @FXML
+    public void menuOpenImage(ActionEvent event){
+        System.out.println("Open file");
+    }
+
+    @FXML
+    public void menuOpenImageWithCoco(ActionEvent event){
+        System.out.println("Open image with metadata");
+    }
+
+    @FXML
+    public void menuSaveData(ActionEvent event){
+        System.out.println("saving to .json files...");
+    }
+
+    @FXML
+    public void menuExit(ActionEvent event){
+
+    }
+
+
+
 }
---
+++ src/sample/SessionInfo.java
@@ -0,0 +1,12 @@
+package sample;
+
+/**
+ * Created by jorda on 3/08/2017.
+ */
+public class SessionInfo {
+
+    public int imageWidth;
+    public int imageHeight;
+    public boolean imageLoaded;
+
+}
