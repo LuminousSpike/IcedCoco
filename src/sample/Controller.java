dc2a2e7dde197f04607cb5a2bf1954f7811c15f1
Jordan <jordanogilvy@live.com>
2017-08-03 17:05:42 +1200

    simple image loading, not scaled

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -8,14 +8,23 @@ import javafx.event.EventTarget;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
+import javafx.scene.image.Image;
 import javafx.scene.text.Text;
+import javafx.stage.FileChooser;
 
 import javax.annotation.Resources;
-import javax.swing.*;
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Observable;
 import java.util.ResourceBundle;
@@ -25,11 +34,11 @@ public class Controller implements Initializable{
     private ResourceBundle resources;
     private SessionInfo sessionInfo = new SessionInfo();
     private Scene scene;
-
     @FXML private MenuBar menuBar;
     @FXML private MenuItem saveMenuItem;
-    @FXML private Menu menu;
-    @FXML private Button button;
+    @FXML private Menu fileMenu;
+    @FXML private Canvas canvas;
+
 
     public void setScene(Scene scene){
         this.scene = scene;
@@ -48,7 +57,26 @@ public class Controller implements Initializable{
     public void menuOpenImage(ActionEvent event){
         // load the image to the canvas without any metadata
         System.out.println("Open file");
+        FileChooser fc = new FileChooser();
+        FileChooser.ExtensionFilter imgFilter = new FileChooser.ExtensionFilter("Images", "*.jpeg", "*.jpg", "*.JPG","*.JPEG", "*.png", "*.PNG", "*.gif", "*.GIF");
+        // a catch all filter incase there is some weird image format we miss
+        FileChooser.ExtensionFilter allFilter = new FileChooser.ExtensionFilter("All Files", "*.*");
+        fc.getExtensionFilters().add(imgFilter);
+        File imgFile = fc.showOpenDialog(scene.getWindow());
+        try {
+            if (imgFile != null) {
+                Image img = new Image(imgFile.toURI().toURL().toExternalForm());       // test this works on all systems
+                GraphicsContext gfx = canvas.getGraphicsContext2D();
+                // we want to fit the canvas to the window size and the image to the canvas size but idk how
+                canvas.setWidth(img.getWidth());
+                canvas.setHeight(img.getHeight());
+                gfx.drawImage(img, 0, 0, canvas.getWidth(), canvas.getHeight());
+            }
+        }catch(MalformedURLException mue){
+            mue.printStackTrace();
+        }
     }
+
 
     @FXML
     public void menuOpenImageWithCoco(ActionEvent event){
