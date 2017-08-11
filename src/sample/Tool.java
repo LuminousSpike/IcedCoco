d65e8bf005939c3e23617628c6b18d0009fe5fb1
Nathan Todd <nathan.j.todd@gmail.com>
2017-08-11 13:52:07 +1200

    Add JavaDocs to Polygon class

--- src/sample/Tool.java
+++ src/sample/Tool.java
@@ -7,10 +7,40 @@ public interface Tool {
     /* Not sure if this is a good design to
      * have a canvas tied to the interface like this.
      * */
+
+    /**
+     * Setter for the tool's canvas.
+     * @param canvas The canvas that the tool will draw on.
+     */
     void setCanvas (Canvas canvas);
+
+    /**
+     * Handles the tool logic for when the mouse enters a drag motion.
+     * @param e Mouse Event for the tool to process.
+     */
     void onDragEntered (MouseEvent e);
+
+    /**
+     * Handles the tool logic for when the mouse is clicked.
+     * @param e Mouse Event for the tool to process.
+     */
     void onMouseClicked (MouseEvent e);
+
+    /**
+     * Handles the tool logic for when the mouse is dragged.
+     * @param e Mouse Event for the tool to process.
+     */
     void onMouseDragged (MouseEvent e);
+
+    /**
+     * Handles the tool logic for when a mouse button is pressed.
+     * @param e Mouse Event for the tool to process.
+     */
     void onMousePressed (MouseEvent e);
+
+    /**
+     * Handles the tool logic for when a mouse button is released.
+     * @param e Mouse Event for the tool to process.
+     */
     void onMouseReleased (MouseEvent e);
 }
