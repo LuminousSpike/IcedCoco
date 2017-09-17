591a8a762fe447d1b71e11994f83bd915eae9a54
Jordan <jordanogilvy@live.com>
2017-09-17 15:39:03 +1200

    can index images, check for existing images in dataset

--- src/sample/SessionInfo.java
+++ src/sample/SessionInfo.java
@@ -12,10 +12,7 @@ import javafx.stage.Popup;
 import javafx.stage.PopupWindow;
 import javafx.stage.Stage;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Optional;
@@ -76,16 +73,16 @@ public class SessionInfo {
         alert.showAndWait();
     }
 
-    private String getNextID(){
+    private long getNextID(){
         // return an available ID for a new image to use, based on whats in the image metadata file
         // currently, IDs are local to each image metadata file.
         // this takes O(n^2) time, idk how to test against all the IDs faster.
         long i = 0;
 
         try {
-
+            FileReader reader = new FileReader((imageDataFile));
             JSONParser parser = new JSONParser();
-            Object obj = parser.parse(new FileReader(imageDataFile));
+            Object obj = parser.parse(reader);
             JSONObject jsonobj = (JSONObject) obj;
 
             JSONArray images = (JSONArray) jsonobj.get("images");
@@ -93,7 +90,8 @@ public class SessionInfo {
             Iterator<JSONObject> iterator = images.iterator();
             ArrayList<String> idlist = new ArrayList<String>();
             while (iterator.hasNext()) {
-                String id = (String) ((JSONObject) iterator.next()).get("id");
+                // read the ids as longs, then add them to the list as strings, to use String equality checking instead of long.
+                String id = Long.toString((long) ((JSONObject) iterator.next()).get("id"));
                 idlist.add(id);
             }
 
@@ -101,6 +99,7 @@ public class SessionInfo {
                 // if this value of i is currently being used as an index. change i
                 ++i;
             }
+            reader.close();
         }catch(FileNotFoundException fnfe){
             // show an alert to say the selected image metadata file can no longer be found.
         }catch(ParseException pe){
@@ -109,27 +108,87 @@ public class SessionInfo {
 
         }
 
-        return Long.toString(i);
+        return i;
     }
 
     private void indexCurrentImage(){
         // add the currently loaded image to the image metadata file with a new ID
         // create entries in the other selected metadata files for the image data, and save.
-        String id = getNextID();
-        String filename = baseImageFile.getName();
-        int license = 0;  // idk how to specify this
-        int width = (int) baseImage.getWidth();
-        int height = (int) baseImage.getHeight();
-        String dateCaptured = "";
-        String cocoURL = "";
+        // these values are ordered here as they should appear in the JSONObject
 
-        // load the entire JSONObject from the image file, add to it, then write it to the file.
+        JSONObject newImage = new JSONObject();
+        newImage.put("license", 0);     // not sure what the license values are
+        newImage.put("file_name", baseImageFile.getName());
+        newImage.put("coco_url", "");
+        newImage.put("height", (int) baseImage.getHeight());
+        newImage.put("width", (int) baseImage.getWidth());
+        newImage.put("id", getNextID());
+        newImage.put("date_captured", ""); // need external library for grabbing image metadata from file - do later.
 
+        // load the entire JSONObject from the image file, add to it, then write it back to the file.
+        JSONParser parser = new JSONParser();
+        try{
+            FileReader reader = new FileReader(imageDataFile);
+            JSONObject masterObject = (JSONObject) parser.parse(reader);
+            reader.close();
+
+            JSONArray images = (JSONArray) masterObject.get("images");
+            images.add(newImage);
+
+            FileWriter writer = new FileWriter(imageDataFile);
+            masterObject.put("images", images);
+            writer.write(masterObject.toJSONString());
+            writer.flush();
+            writer.close();
+
+        }catch(FileNotFoundException fnfe){
+
+        }catch(ParseException pe){
+
+        }catch(IOException ioe){
+
+        }
     }
+
+    private boolean currentImageIndexed(){
+        // return true if the current image in the editor is indexed in the selected image metadata file
+        // assume baseImage, imageDataFile are not null
+        boolean output = false;
+
+        JSONParser parser = new JSONParser();
+        try {
+            FileReader reader = new FileReader(imageDataFile);
+            JSONObject object = (JSONObject) parser.parse(reader);
+            reader.close();
+            JSONArray images = (JSONArray) object.get("images");
+            String currentImageName = baseImageFile.getName();
+
+            // check each image. If the file_name attributes match, our image is indexed, return true.
+            // otherwise falls out and returns false.
+            Iterator<JSONObject> iterator = images.iterator();
+            while (iterator.hasNext()) {
+                JSONObject img = iterator.next();
+                String name = (String) img.get("file_name");
+                if(name.equals(currentImageName)){
+                    output = true;
+                    break;
+                }
+            }
+
+        }catch(FileNotFoundException fnfe){
+
+        }catch(ParseException pe){
+
+        }catch(IOException ioe){
+
+        }
+        return output;
+    }
+
 
     public void checkImageMetadata(){
         // metadata handling, called when a new image is loaded to the canvas, or the selected metadata files change.
-        // do nothing if image null
+        // do nothing if image is null
         if (baseImage==null){
             return;
         }
@@ -137,10 +196,11 @@ public class SessionInfo {
         if(imageDataFile==null){
             // show pop up warning
             showNoMetadataSelectedDialog();
+            return;
         }
         // case 2 : metadata file selected, image is not indexed in the metadata file.
-        else if (true){
-            // pop up? to add this image to the metadata files
+        else if (!currentImageIndexed()){
+            // show a pop up prompt to add this image to the metadata files
             boolean doIndex = showNotIndexedDialog();
             if (doIndex){
                 // index the image in the current json image data json file, and any other selected coco files
