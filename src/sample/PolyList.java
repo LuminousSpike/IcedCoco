0867d6fef851dc5d24e0124a64e3caddc4667efe
Nathan Todd <nathan.j.todd@gmail.com>
2017-09-25 01:51:55 +1300

    Fix build issue

--- src/sample/PolyList.java
+++ src/sample/PolyList.java
@@ -69,6 +69,10 @@ public class PolyList {
         this.selectedVertex = selectedVertex;
     }
 
+    public LinkedList<Polygon> getPolygons() {
+        return polygons;
+    }
+
     public void add(Polygon polygon) {
         polygons.add(polygon);
     }
