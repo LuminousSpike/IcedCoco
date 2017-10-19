b66981d25e409b1cb2b5c261a7fdeba9b6eb19cc
Nathan Todd <nathan.j.todd@gmail.com>
2017-10-19 16:43:29 +1300

    Implement saving and loading support
    For SettingsInfo and HotkeysInfo.
    
    Not writing to file as of yet, it just prints out to console.

--- src/main/java/com/limbo/icedcoco/SettingsInfo.java
+++ src/main/java/com/limbo/icedcoco/SettingsInfo.java
@@ -1,7 +1,12 @@
 package com.limbo.icedcoco;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
+import java.io.File;
 
 @XmlRootElement (name = "settings")
 public class SettingsInfo {
@@ -26,4 +31,43 @@ public class SettingsInfo {
     @XmlElement boolean useDefaultSaveLocation;
     @XmlElement String defaultSaveLocation;
     @XmlElement OptionFrequency checkForUpdates, reopenLastFile, reopenLastDirectory, automaticallyExport;
+
+    void save (String filepath) {
+        try {
+            JAXBContext jc = JAXBContext.newInstance("com.limbo.icedcoco");
+
+            Marshaller m = jc.createMarshaller();
+            m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+            m.marshal(this, System.out);
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
+    }
+
+    static SettingsInfo load (String filepath) {
+        try {
+            File file = new File(filepath);
+            if (file.exists()) {
+                JAXBContext jaxbContext = JAXBContext.newInstance("com.limbo.icedcoco");
+                Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+                SettingsInfo settingsInfo = (SettingsInfo) unmarshaller.unmarshal(file);
+                return settingsInfo;
+            }
+        }
+        catch (Exception ex) {
+            System.err.println(ex.getStackTrace());
+        }
+        return null;
+    }
+
+    void loadDefault () {
+        defaultSaveLocation = "/How/Are/You/";
+        showHelpDialogOnStartup = false;
+        showHelpDialogOnUpdate = true;
+        showMetaDataWarning = true;
+        checkForUpdates = SettingsInfo.OptionFrequency.Always;
+        reopenLastFile = SettingsInfo.OptionFrequency.Ask;
+        reopenLastDirectory = SettingsInfo.OptionFrequency.Ask;
+        automaticallyExport = SettingsInfo.OptionFrequency.Never;
+    }
 }
